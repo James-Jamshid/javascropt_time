47f7596d9d2495e8ec7be616b262fa6937b62d8d
Jamshidbek Makhmudov <101079499+dvJamshidbek@users.noreply.github.com>
2022-05-30 14:30:14 +0900

    created

---
+++ javscript_time.js
@@ -0,0 +1,39 @@
+//day and time
+
+
+
+
+
+
+var today= new Date().getDay()
+var daylist=['Sunday','Monday','Tuesday','Wednessday','Thusday','Friday','Saturday']
+console.log(`Today is ${daylist[today]}.`);
+
+
+var hour=new Date().getHours()
+var minute=new Date().getMinutes()
+var second=new Date().getSeconds()
+var prepand=(hour>=12)? ' PM' : ' AM'
+hour=(hour>=12)? hour-12 :hour
+if(hour===0 && prepand==='PM'){
+    if(minute===0 && second===0){
+        hour=12
+        prepand='Noon'
+    }
+    else{
+        hour=12
+        prepand='PM'
+    }
+
+}
+if(hour===0 && prepand==='AM'){
+    if(minute===0 &&second===0){
+        hour=12
+        prepand="Midnight"
+    }
+    else{ hour=12
+        prepand='AM'
+
+    }
+}
+console.log(`Current Time: ${hour} ${prepand} : ${minute} :${second}`);
